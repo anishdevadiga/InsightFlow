bd6ee6724f13c3cb2807331bf13fb8904e30ec19
unknown <anishkdevadiga2002@gmail.com>
2025-08-26 20:25:15 +0530

    completed describe module

--- modules/describe_module.py
+++ modules/describe_module.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+#providing description for the module
+
 def describe_dataset(df:pd.DateFrame):
     st.subheader("Dataset Overview")
     rows,cols=df.shape
@@ -8,4 +10,11 @@ def describe_dataset(df:pd.DateFrame):
     st.write(f"-Number of columns :{cols}")
     st.write(f"-Column names:{list(df.columns)}")
 
-    st
+    st.markdown("--Missing value--")
+    st.dataframe(df.isna().sum().to_frame("missing_count"))
+
+    st.markdown("--Statistical Summary--")
+    st.dataframe(df.describe(include='all'))
+
+    with st.expander("Show first 10 rows"):
+        st.dataframe(df.head(10))
